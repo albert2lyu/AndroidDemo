6e97f0732b24705bde358a3591212ec7cbd8e2d8
mjj-work <tyzl931019@qq.com>
2015-10-22 16:33:15 +0800

    sqlite 2

--- SQLite/app/src/main/java/com/mjj/sqlite/MainActivity.java
+++ SQLite/app/src/main/java/com/mjj/sqlite/MainActivity.java
@@ -4,14 +4,21 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private MySQLiteOpenHelper MySQLiteOpenHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MySQLiteOpenHelper = new MySQLiteOpenHelper(this,"BookStore.db",null,3);
+    }
+
+    public void getDatabase(View v){
+        MySQLiteOpenHelper.getReadableDatabase();
     }
 
     @Override
@@ -27,7 +34,6 @@ public class MainActivity extends ActionBarActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
--- SQLite/app/src/main/java/com/mjj/sqlite/MySQLiteOpenHelper.java
+++ SQLite/app/src/main/java/com/mjj/sqlite/MySQLiteOpenHelper.java
@@ -4,10 +4,45 @@ import android.content.Context;
 import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
-
+/**
+ * 构建实例之后，再调用getReadableDatabase()或者getWirteableDatabase()，就会创建数据库。
+ * 数据库文件存放在/data/data/<包名>/database/
+ * 如果数据库不存在，会默认去调用onCreate()方法。
+ *
+ * 可用的数据类型：
+ * integer  整型
+ * real     浮点型
+ * text     文本类型
+ * blob     二进制类型
+ */
 public class MySQLiteOpenHelper extends SQLiteOpenHelper {
 
+    /**
+     * 创建Book表的SQL语句
+     * primary key      表示主键
+     * autoincrement    表示自增长
+     */
+    private static final String CREATE_BOOK = "create table Book (" +
+                "id integer primary key autoincrement," +
+                "author text," +
+                "price real," +
+                "pages integer," +
+                "name text)";
+
+    private static final String CREATE_CATEGORY = "create table Category(" +
+                "id integer primary key autoincrement," +
+                "category_name text," +
+                "category_code integer)";
+
+    /**
+     * 构造方法
+     * @param context   上下文
+     * @param name      数据库名
+     * @param factory   自定义的Cursor,一般为null
+     * @param version   数据库版本号
+     */
     public MySQLiteOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
@@ -16,25 +51,45 @@ public class MySQLiteOpenHelper extends SQLiteOpenHelper {
         super(context, name, factory, version, errorHandler);
     }
 
+    /**
+     * 数据库不存在时执行，创建数据库
+     * @param sqLiteDatabase
+     */
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+        sqLiteDatabase.execSQL(CREATE_BOOK);
+        sqLiteDatabase.execSQL(CREATE_CATEGORY);
+        Log.i("asd","创建了数据库");
     }
 
     /**
-     *
-     * @return
+     * 当传入的数据库版本号大于旧版本号时执行，更新数据库。
+     * @param sqLiteDatabase
+     * @param oldVersion    旧版本号
+     * @param newVersion    新版本号
+     */
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        //先删除表
+        sqLiteDatabase.execSQL("drop table if exists Book");
+        sqLiteDatabase.execSQL("drop table if exists Category");
+        onCreate(sqLiteDatabase);
+        Log.i("asd","更新了数据库");
+    }
+
+    /**
+     * 当数据库不可写入时（如磁盘空间已满）返回的对象以只读的形式打开数据库
+     * @return SQLiteDatabase 返回一个可读写的数据库对象（如果数据库已存在则直接打开，否则创建一个新的数据库）
      */
     @Override
     public SQLiteDatabase getReadableDatabase() {
         return super.getReadableDatabase();
     }
 
+    /**
+     * 当数据库不可写入时（如磁盘空间已满）将出现异常
+     * @return SQLiteDatabase 返回一个可读写的数据库对象（如果数据库已存在则直接打开，否则创建一个新的数据库）
+     */
     @Override
     public SQLiteDatabase getWritableDatabase() {
         return super.getWritableDatabase();
